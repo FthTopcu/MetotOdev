d18221d89f0b6c0dbcaa71276df367a73b99f810
FthTopcu <fthtpu@gmail.com>
2021-11-23 17:40:22 +0300

    Add project files.

---
+++ MetotOdev/Matematik.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotOdev
+{
+    class Matematik
+    {
+        public bool Yeniden()
+        {
+            Console.WriteLine("Devam etmek istiyor musunuz.(E/H)");
+            string secim = Console.ReadLine();
+            if (secim.ToLower() == "e")
+                return true;
+            else
+            {
+                return false;
+            }
+
+        }
+        public void Menu()
+        {
+
+            Console.WriteLine("Menu");
+            Console.WriteLine("1-Toplama");
+            Console.WriteLine("2-Çıkartma");
+            Console.WriteLine("3-Çarpma");
+            Console.WriteLine("4-Bölme");
+            Console.Write("Lütfen bir değer seçiniz :");
+            int secim = Convert.ToInt32(Console.ReadLine());
+
+            switch (secim)
+            {
+                case 1:
+                    Console.WriteLine("\nToplama Sectiniz");
+
+                    Topla(sayiGir(), sayiGir());
+                    break;
+                case 2:
+                    Console.WriteLine("\nÇıkartma Sectiniz");
+                    Cikart(sayiGir(), sayiGir());
+                    break;
+                case 3:
+                    Console.WriteLine("\nÇarpma Sectiniz");
+                    Carp(sayiGir(), sayiGir());
+                    break;
+                case 4:
+                    Console.WriteLine("\nBölme Sectiniz");
+                    Bol(sayiGir(), sayiGir());
+                    break;
+                default:
+                    Console.WriteLine("\nYanlış değer girdiniz.");
+                    break;
+            }
+        }
+        private static int sayiGir()
+        {
+            Console.Write("Sayı girinz : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            return x;
+
+        }
+        private static void Topla(double x, double y)
+        {
+
+            double sonuc = x + y;
+            EkranaYazdir("+", x, y, sonuc);
+        }
+        private static void Cikart(double x, double y)
+        {
+            double sonuc = x - y;
+            EkranaYazdir("-", x, y, sonuc);
+        }
+        private static void Carp(double x, double y)
+        {
+            double sonuc = x * y;
+            EkranaYazdir("*", x, y, sonuc);
+        }
+        private static void Bol(double x, double y)
+        {
+            double sonuc = x / y;
+            sonuc = Math.Round(sonuc,2);
+            EkranaYazdir("/", x, y, sonuc);
+        }
+        private static void EkranaYazdir(string islem, double x, double y, double sonuc)
+        {
+            Console.WriteLine(x + " " + islem + " " + y + " = " + sonuc);
+        }
+    }
+}
---
+++ MetotOdev/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotOdev
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region 
+            /*
+             matematik adında class oluşturalacak
+             bir menü hazırlanıcak metot ile
+             menüde 
+             1-toplama
+             2-çıkartma
+             3-bölme
+             4-çarpma
+             lütfen bir değer seciniz
+             
+             */
+            #endregion
+            Matematik mat = new Matematik();
+            
+            bool giris = true;
+            while (giris)
+            {
+                mat.Menu();
+                
+                giris=mat.Yeniden();
+                Console.Clear();
+            }
+        }
+    }
+
+    
+}
